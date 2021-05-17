1eb0a25fd8a1903fa7278851bb8de412f638a584
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-17 14:42:44 -0500

    git init

---
+++ src/main/java/com/codeup/tier1hire/Tier1hireApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.tier1hire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tier1hireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tier1hireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/tier1hire/Tier1hireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.tier1hire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tier1hireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
