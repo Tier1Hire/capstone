8a50e6a479508910ac24fea652e526a20c7beb77
audiesha brattin <a.brattin@gmail.com>
2021-05-21 16:35:13 -0500

    debugging

--- src/main/java/com/codeup/tier1hire/controllers/CreateProfileController.java
+++ src/main/java/com/codeup/tier1hire/controllers/CreateProfileController.java
@@ -1,76 +1,72 @@
 package com.codeup.tier1hire.controllers;
   import com.codeup.tier1hire.repositories.NewProfileRepo;
-  import org.springframework.boot.SpringApplication;
-  import org.springframework.boot.autoconfigure.SpringBootApplication;
+  import org.apache.tomcat.jni.User;
   import org.springframework.core.env.Profiles;
   import org.springframework.stereotype.Controller;
   import org.springframework.ui.Model;
   import org.springframework.web.bind.annotation.*;
 
-  import java.io.File;
-  import java.util.ArrayList;
   import java.util.List;
 
 @Controller
 public class CreateProfileController {
 
 
-  private final NewProfileRepo postDao;
+  private final NewProfileRepo userDao;
 
-  public CreateProfileController(NewProfileRepo postDao){
-    this.postDao = postDao;
+  public CreateProfileController(NewProfileRepo userDao){
+    this.userDao = userDao;
   }
 
   @GetMapping("/createProfile")
   public String allPosts(Model model){
 
-    model.addAttribute("profile", postDao.findAll());
+    model.addAttribute("profile", userDao.findAll());
     return "posts/index";
   }
 
   @GetMapping("profiles/search")
   public String searchPosts(@RequestParam(name = "term") String term, Model viewModel){
     term = "%"+term+"%";
-    List<Profiles> dbPosts = postDao.findAllByTitleIsLike(term);
+    List<Profiles> dbPosts = userDao.findAllByTitleIsLike(term);
     viewModel.addAttribute("posts", dbPosts);
-    return "posts/index";
+    return "redirect:/profile";
   }
 
   @GetMapping("/profiles/{id}")
   public String viewSelectedPost(@PathVariable long id, Model model){
 
-    model.addAttribute("selectedPost", postDao.getOne(id));
-    return "posts/show";
+    model.addAttribute("selectedPost", userDao.getOne(id));
+    return "redirect:/profile";
   }
 
   @GetMapping("/profile/create")
   public String createNewPost(Model model){
-    return "posts/new";
+    return "profile/new";
   }
 
   @PostMapping("/profile/create")
-  public String savingNewPost(@ModelAttribute Post post){
-    postDao.save(post);
-    return "redirect:/posts/" + post.getId();
+  public String savingNewPost(@ModelAttribute User user){
+    userDao.save(user);
+    return "redirect:/profile/" + user.getId();
   }
 
   @GetMapping("/profile/delete/{id}")
   public String deletePost(@PathVariable long id){
-    Post post = postDao.getOne(id);
-    postDao.delete(post);
-    return "redirect:/posts";
+    User userDao = user.getOne(id);
+    userDao.delete(userDao);
+    return "redirect:/profile";
   }
-
-  @GetMapping("/profile/edit/{id}")
-  public String editAd(@PathVariable long id, Model model) {
-    Post post = postDao.getOne(id);
-    model.addAttribute("post", post);
-    return "posts/create";
+  @GetMapping("/profile/{id}/edit")
+  public String updateProfile(@PathVariable long id, Model model ){
+    User updateUser = (User) userDao.getOne(id);
+    model.addAttribute ("profile",updateUser);
+    return "profile/edit";
   }
 
   @PostMapping("/profile/edit")
-  public String updateAd(@ModelAttribute Post post) {
-    postDao.save(post);
-    return "redirect:/posts/" + post.getId();
+  public String updateUser(@ModelAttribute User user) {
+    userDao.save(user);
+    return "redirect:/profile" + user.getId();
   }
 }
