09fab3a5da3a387045f12fa6106a701a79fb60b3
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-24 11:21:32 -0500

    linking the pricing controller

--- src/main/java/com/codeup/tier1hire/controllers/PricingController.java
+++ src/main/java/com/codeup/tier1hire/controllers/PricingController.java
@@ -1,4 +1,13 @@
 package com.codeup.tier1hire.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PricingController {
+
+    @GetMapping("/pricing")
+    public String pricing() {
+        return "/pricing";
+    }
 }
