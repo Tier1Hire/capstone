cac853a82498e9b883d2239d3338d59e206d1d24
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-05-23 22:41:02 -0500

    secuirty config

--- src/main/java/com/codeup/tier1hire/SecurityConfiguration.java
+++
@@ -1,7 +0,0 @@
-//package com.codeup.tier1hire;
-//
-//import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
-//
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//}
