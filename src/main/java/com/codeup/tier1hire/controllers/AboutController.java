fe7307ee57774247d084133547c768d80b644280
WilliamBaldridge <baldridge.william09@gmail.com>
2021-05-24 10:30:53 -0500

    feat: about page controller

--- src/main/java/com/codeup/tier1hire/controllers/AboutController.java
+++ src/main/java/com/codeup/tier1hire/controllers/AboutController.java
@@ -1,4 +1,15 @@
 package com.codeup.tier1hire.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AboutController {
+
+    @GetMapping("/about")
+    public String about() {
+
+        return "about";
+    }
+
 }
