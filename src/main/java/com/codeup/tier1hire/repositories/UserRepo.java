bc4bc2280163f25d47bf7c86b30f655b2f4ade45
WilliamBaldridge <baldridge.william09@gmail.com>
2021-05-25 11:58:31 -0500

    fixing and adding custom queries for searching

--- src/main/java/com/codeup/tier1hire/repositories/UserRepo.java
+++ src/main/java/com/codeup/tier1hire/repositories/UserRepo.java
@@ -3,10 +3,15 @@ package com.codeup.tier1hire.repositories;
 import com.codeup.tier1hire.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
-    @Query("select u from User u where u.username = ?1")
-    User findByUsername(String username);
+    @Query("FROM User u where u.username LIKE %:term%")
+    List<User> getUserByUsername(@Param("term")String term);
+
+
 
 }
