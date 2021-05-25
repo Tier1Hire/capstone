37c3881a0114fdc2f1db71921d9c8e2e4303ca23
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-25 09:17:10 -0500

    feat: adding hashpassword to user controller

--- src/main/java/com/codeup/tier1hire/controllers/UserController.java
+++ src/main/java/com/codeup/tier1hire/controllers/UserController.java
@@ -4,10 +4,12 @@ import com.codeup.tier1hire.models.User;
 import com.codeup.tier1hire.repositories.EducationDetailRepo;
 import com.codeup.tier1hire.repositories.EmploymentDetailRepo;
 import com.codeup.tier1hire.repositories.UserRepo;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.PasswordAuthentication;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,11 +18,13 @@ public class UserController {
     private final UserRepo usersDao;
     private final EducationDetailRepo educationDao;
     private final EmploymentDetailRepo employmentDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepo usersDao, EducationDetailRepo educationDao, EmploymentDetailRepo employmentDao) {
+    public UserController(UserRepo usersDao, EducationDetailRepo educationDao, EmploymentDetailRepo employmentDao, PasswordEncoder passwordEncoder) {
         this.usersDao = usersDao;
         this.educationDao = educationDao;
         this.employmentDao = employmentDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/users")
@@ -36,13 +40,21 @@ public class UserController {
         return "/profile";
     }
 
-    @GetMapping("users/register")
-    public String createUser(@ModelAttribute User user) {
-        usersDao.save(user);
-        return "/profile";
+    @GetMapping("/users/register")
+    public String createUser(Model model) {
+        model.addAttribute("user", new User());
+        return "users/create";
     }
 
-    @GetMapping("users/{id}/edit")
+    @PostMapping("/users/register")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersDao.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/users/{id}/edit")
     public String updateUser(@PathVariable long id, Model model) {
         User updateUser = usersDao.getOne(id);
         model.addAttribute("users", updateUser);
@@ -50,10 +62,8 @@ public class UserController {
     }
 
     @PostMapping("users/edit/{id}")
-    public String updateUser(@ModelAttribute User user) {
-        LocalDateTime dateOfBirth;
-        LocalDateTime createdAt;
-        usersDao.save(user);
+    public String updateUser(@ModelAttribute User updateUser) {
+        usersDao.save(updateUser);
         return "redirect:/profile";
     }
 
--- src/main/java/com/codeup/tier1hire/repositories/UserRepo.java
+++ src/main/java/com/codeup/tier1hire/repositories/UserRepo.java
@@ -2,8 +2,11 @@ package com.codeup.tier1hire.repositories;
 
 import com.codeup.tier1hire.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where u.username = ?1")
     User findByUsername(String username);
 
 }
