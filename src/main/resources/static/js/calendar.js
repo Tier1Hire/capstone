221d4728d7d9364140cf9d2ee8337a56bc27c2e5
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-27 14:40:17 -0500

    doing stuff with the calendar api

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,65 +1,36 @@
 // "use strict";
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        initialDate: '2021-05-07',
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2021-05-01'
+// document.addEventListener('DOMContentLoaded', function() {
+//     var calendarEl = document.getElementById('calendar');
+//     var calendar = $(this);
+//     calendar.fullCalenar({
+//         initialView: 'dayGridMonth',
+//         initialDate: '2021-05-07',
+//         headerToolbar: {
+//             left: 'prev,next today',
+//             center: 'title',
+//             right: 'dayGridMonth,timeGridWeek,timeGridDay'
+//         },
+//     });
+//     calendar.render();
+//     calendar.setOption('contentHeight', 650);
+// });
+
+let initialize_calendar;
+initialize_calendar = function () {
+    $('.calendar').each(function () {
+        let calendar = $(this);
+        calendar.fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
             },
-            {
-                title: 'Long Event',
-                start: '2021-05-07',
-                end: '2021-05-10'
-            },
-            {
-                groupId: '999',
-                title: 'Repeating Event',
-                start: '2021-05-09T16:00:00'
-            },
-            {
-                groupId: '999',
-                title: 'Repeating Event',
-                start: '2021-05-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2021-05-11',
-                end: '2021-05-13'
-            },
-            {
-                title: 'Meeting',
-                start: '2021-05-12T10:30:00',
-                end: '2021-05-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2021-05-12T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2021-05-12T14:30:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2021-05-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2021-05-28'
-            }
-        ]
-    });
-    calendar.render();
-    calendar.setOption('contentHeight', 650);
-});
+            selectable: true,
+            selectHelper: true,
+            editable: true,
+            eventLimit: true
+        })
+    })
+};
