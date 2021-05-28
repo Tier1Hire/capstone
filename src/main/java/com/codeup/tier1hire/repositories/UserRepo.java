9798643cf4254843daa15d3701b340e626e50b1c
WilliamBaldridge <baldridge.william09@gmail.com>
2021-05-28 14:25:05 -0500

    Search by first and last names, and by school

--- src/main/java/com/codeup/tier1hire/repositories/UserRepo.java
+++ src/main/java/com/codeup/tier1hire/repositories/UserRepo.java
@@ -2,8 +2,19 @@ package com.codeup.tier1hire.repositories;
 
 import com.codeup.tier1hire.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByUsername(String username);
+
+    List<User> findAllByFirstNameIsLikeOrLastNameIsLike(String firstName, String lastName);
+
+    @Query("FROM User u JOIN u.education e WHERE e.school = ?1")
+    List<User> findAllByEducation(String school);
+
+    @Query("FROM ")
+
 }
