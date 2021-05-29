f2c493351d669a7f68ce8b5fe5e8a8f4b23dfe25
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-29 11:42:24 -0500

    feat: making changes to the job experience controller

--- src/main/java/com/codeup/tier1hire/controllers/JobExperienceController.java
+++ src/main/java/com/codeup/tier1hire/controllers/JobExperienceController.java
@@ -1,10 +1,14 @@
 package com.codeup.tier1hire.controllers;
 
 import com.codeup.tier1hire.models.EmploymentDetail;
+import com.codeup.tier1hire.models.User;
 import com.codeup.tier1hire.repositories.EmploymentDetailRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobExperienceController {
@@ -20,4 +24,31 @@ public class JobExperienceController {
         model.addAttribute("employment_detail", new EmploymentDetail());
         return "job-experience";
     }
+
+    @PostMapping("/job-experience")
+    public String addJobExperienceForm(@ModelAttribute("employment_detail") EmploymentDetail employmentDetail) {
+        employmentDetailDao.save(employmentDetail);
+        return "redirect:/users/profile";
+    }
+
+
+
+    @GetMapping("/job-experience")
+    public String getOneEmployment(Model model) {
+        EmploymentDetail employmentDetail = (EmploymentDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("employment_detail", employmentDetail);
+        return "job-experience";
+    }
+
+    @PostMapping("/job-experience")
+    public String updateEmployment(@ModelAttribute("employment_detail") EmploymentDetail employmentDetail) {
+        EmploymentDetail updatedEmployment = employmentDetailDao.findById(employmentDetail.getEmploymentId()).get();
+        updatedEmployment.setEmployer(employmentDetail.getEmployer());
+        updatedEmployment.setAddress1(employmentDetail.getAddress1());
+        updatedEmployment.setAddress2(employmentDetail.getAddress2());
+        updatedEmployment.setPositions(employmentDetail.getPositions());
+        employmentDetailDao.save(updatedEmployment);
+        return "users/profile";
+    }
+
 }
