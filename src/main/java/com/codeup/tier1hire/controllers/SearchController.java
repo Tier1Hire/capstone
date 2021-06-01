5c44f036a77818680903f4ca6fe5f33d6e82f909
WilliamBaldridge <baldridge.william09@gmail.com>
2021-06-01 11:15:09 -0500

    search bar linked to query

--- src/main/java/com/codeup/tier1hire/controllers/SearchController.java
+++ src/main/java/com/codeup/tier1hire/controllers/SearchController.java
@@ -2,11 +2,13 @@ package com.codeup.tier1hire.controllers;
 
 import com.codeup.tier1hire.models.User;
 import com.codeup.tier1hire.repositories.UserRepo;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SearchController {
@@ -17,14 +19,21 @@ public class SearchController {
         this.usersDao = usersDao;
     }
 
-    @GetMapping("/search/{keyword}")
-    public String search(@PathVariable String keyword, Model model) {
+//    @GetMapping("search/{keyword}")
+//    public String searchResults(@PathVariable String keyword, Model model) {
+//
+//        model.addAttribute("users", usersDao.search(keyword));
+//        return "search-results";
+//    }
 
+    @PostMapping("/search")
+    public String search(@Param("keyword") String keyword, Model model) {
+
+        model.addAttribute("keyword", keyword);
         model.addAttribute("users", usersDao.search(keyword));
-//        model.addAttribute("education", );
-//        model.addAttribute("employment", );
-        return "/search-results";
+        return "search-results";
     }
+
 
     //    // Must make separate views for results as using same seems to throw "ambiguous error"
 //    @GetMapping("/search/{name}")
